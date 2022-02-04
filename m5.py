4e5596c9a751dfaf39246c53ffa9b7f9f4d667e1
guguldash <84378775+guguldash@users.noreply.github.com>
2022-02-04 11:48:16 +0530

    Add files via upload

---
+++ m5.py
@@ -0,0 +1,15 @@
+name=[ 'n', 'i', 't', 'i', 'n' ]
+i=-1
+a=[]
+while i>=(-len(name)):
+    a.append(name[i])
+    i=i-1
+if name==a:
+     print("its planidrom")
+else:
+    print("not") 
+        
+
+
+    
+
